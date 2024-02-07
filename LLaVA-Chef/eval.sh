f069d8cc611cf422e83edf4580946babc0c34c32
mohbattharani <mohbattharani1@gmail.com>
2024-02-06 22:19:52 -0500

    prompts and new results added

--- LLaVA-Chef/eval.sh
+++ LLaVA-Chef/eval.sh
@@ -2,6 +2,17 @@
 
 
 python eval.py \
+    --model_path="/data/checkpoints/LLaVA/LLaVA-Chef/LLaVA-Chef-S3/"\
+    --dataset_dir=/data/datasets/Food/Recipe1M/ \
+    --save_dir=./results/ \
+    --eval_type="im_t" \
+    --model_type="LLaVAChefS3" \
+    --partition="test_1kimage" \
+    --prompt_type="i__ing" \
+    --start=0 \
+    --end=-1
+'''
+python eval.py \
     --model_path="/data/checkpoints/LLaVA/LLaVA-7B-Lightening-v1-1/"\
     --dataset_dir=/data/datasets/Food/Recipe1M/ \
     --save_dir=./results/ \
@@ -47,3 +58,5 @@ python eval.py \
     --prompt_type="ing_i_t__instruct" \
     --start=0 \
     --end=-1
+
+'''
